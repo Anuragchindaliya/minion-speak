3e995b081b671f6aa049eefe6181965733eecc89
AnuragChindaliya <ak1148383@gmail.com>
2021-06-06 18:33:05 +0530

    minion speak translator

---
+++ assets/script.js
@@ -0,0 +1,46 @@
+var btnTranslate = document.querySelector("#btnTranslate");
+var txtinput = document.querySelector("#txt-input");
+var output = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+var error_msg = document.querySelector("#error_msg")
+var input_error = document.querySelector("#input_error")
+
+
+function constructURL(text) {
+  return serverURL + "?text=" + text;
+}
+function errorhandler(error) {
+  
+  console.log("error occurred ", error);
+  alert("something went wrong with server! try again after some time");
+}
+
+function handleClickEvent() {
+  //   output.innerText = "bananan lahanuag " + txtinput.value;
+
+  var inputTxt = txtinput.value; //taking input
+  if ((inputTxt == "")) {
+    input_error.innerText="Please fill this input box";
+    setTimeout(()=>input_error.innerText="",4000)
+    
+    // setInterval(()=>input_error.innerText="",2000);
+    
+  } else {
+    fetch(constructURL(inputTxt)) //process
+      .then((response) => response.json())
+      .then(
+        (json) => {
+          if(json.contents){
+            output.textContent = json.contents.translated
+          }else{
+            output.textContent = json.error.message
+          }
+          
+         } //output
+        // (json) => console.log(json.contents.translated)
+      )
+      .catch(errorhandler);
+  }
+}
+btnTranslate.addEventListener("click", handleClickEvent);
+
